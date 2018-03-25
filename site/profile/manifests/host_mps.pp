cd752318857bc8dc8ad9e5ed1b78bea20de28b78
PowerSix <sinisam@gmail.com>
2018-03-25 21:40:04 +0000

    commit

--- site/profile/manifests/host_mps.pp
+++ site/profile/manifests/host_mps.pp
@@ -1,5 +1,8 @@
-class profile::mps_host {
+class profile::host_mps {
   host { 'www.six.com':
     ip => '52.49.69.27',
+  }
+  host { 'www.google.com':
+    ip => '127.0.0.1',
   }
 }
--- site/profile/manifests/website_content.pp
+++ site/profile/manifests/website_content.pp
@@ -1,6 +1,7 @@
 class profile::website_content {
-  file { 'c:\\inetpub\\minimal\\content' :
+  file { 'c:\\inetpub\\minimal\\content':
     ensure => directory,
+    recurse => remote,
     source  => 'puppet:///modules/profile/demo-website/',
   }
 }
