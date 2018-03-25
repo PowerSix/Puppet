8cc9841fa358845a7eacc2cb6f728f7b5a2cb41f
PowerSix <sinisam@gmail.com>
2018-03-25 23:13:29 +0000

    Fixed groups

--- site/profile/manifests/windows_defaults.pp
+++ site/profile/manifests/windows_defaults.pp
@@ -1,16 +1,21 @@
 class profile::windows_defaults {
+
   host { 'www.six.com':
     ip => '52.49.69.27',
   }
+
   host { 'www.google.com':
     ip => '127.0.0.1',
   }
+
   user { 'Six':
-    ensure => present,
-    comment => 'Managed by Puppet'
-    groups => 'Administrators',
-    password => '1stPassword',
+    ensure     => present,
+    comment    => 'Managed by Puppet'
+    groups     => ['Administrators'],
+    membership => inclusive,
+    password   => '1stPassword',
   }
+
   $defaults_message = @("CONTENT")
     Windows defaults applied.
     | CONTENT
