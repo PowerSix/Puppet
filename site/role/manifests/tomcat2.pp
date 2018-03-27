e85fd5dda3d72e9d69cb7f80344743c7c73702d1
PowerSix <sinisam@gmail.com>
2018-03-28 00:50:39 +0100

    tomcat 2

---
+++ site/role/manifests/tomcat2.pp
@@ -0,0 +1,16 @@
+class role::tomcat2 {
+  exec { 'install_choco':
+    command  => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))',
+    provider => powershell,
+  } ->
+
+  exec { 'choco_jdk8':
+    command  => '$(choco install jdk8 -y',
+    provider => powershell,
+  } ->
+
+  exec { 'choco_tomcat':
+    command  => '$(choco install tomcat -y',
+    provider => powershell,
+  }
+}
