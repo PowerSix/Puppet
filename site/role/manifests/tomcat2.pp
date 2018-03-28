ef447d8af6388f4ce7a3fbfbb6d98e2c326bb1d6
PowerSix <sinisam@gmail.com>
2018-03-28 01:12:36 +0100

    Choco fix

--- site/role/manifests/tomcat2.pp
+++ site/role/manifests/tomcat2.pp
@@ -1,18 +1,19 @@
 class role::tomcat2 {
   exec { 'install_choco':
-    command   => '$(Set-ExecutionPolicy Bypass -Scope Process -Force; start-transcript c:\\users\\administrator\\desktop\\transcript.txt; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); exit 0)',
+    # command   => '$(Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); exit 0)',
+    command   => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); exit 0)',
     provider  => powershell,
     logoutput => true,
   } ->
 
   exec { 'choco_jdk8':
-    command   => '$(choco install jdk8 -y',
+    command   => '$(choco install jdk8 -y)',
     provider  => powershell,
     logoutput => true,
   } ->
 
   exec { 'choco_tomcat':
-    command   => '$(choco install tomcat -y',
+    command   => '$(choco install tomcat -y)',
     provider  => powershell,
     logoutput => true,
   }
