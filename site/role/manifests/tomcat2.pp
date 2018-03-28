64ee9cc37531e929cd5c012031d025bfa27554f1
PowerSix <sinisam@gmail.com>
2018-03-28 01:19:25 +0100

    Choco fix

--- site/role/manifests/tomcat2.pp
+++ site/role/manifests/tomcat2.pp
@@ -1,18 +1,19 @@
 class role::tomcat2 {
   exec { 'install_choco':
-    command   => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\\ProgramData\\chocolatey\\bin", [EnvironmentVariableTarget]::Process); Exit 0)',
+    # command   => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\\ProgramData\\chocolatey\\bin", [EnvironmentVariableTarget]::Process); Exit 0)',
+    command   => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); Exit 0)',
     provider  => powershell,
     logoutput => true,
   } ->
 
   exec { 'choco_jdk8':
-    command   => '$(choco install jdk8 -y)',
+    command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install jdk8 -y)',
     provider  => powershell,
     logoutput => true,
   } ->
 
   exec { 'choco_tomcat':
-    command   => '$(choco install tomcat -y)',
+    command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install tomcat -y)',
     provider  => powershell,
     logoutput => true,
   }
