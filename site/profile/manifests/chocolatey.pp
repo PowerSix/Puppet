b61d23da373f8e47978a7d2f812a659b6ad74000
PowerSix <sinisam@gmail.com>
2018-03-29 16:42:11 +0100

    commit

--- site/profile/manifests/chocolatey.pp
+++ site/profile/manifests/chocolatey.pp
@@ -1,7 +1,8 @@
 class profile::chocolatey {
   exec { 'chocolatey':
     command   => '$(Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); Exit 0)',
-    unless    => '$(Test-Path -Path C:\\ProgramData\\chocolatey\\bin\\choco.exe)',
+    # unless    => '$(Test-Path -Path C:\\ProgramData\\chocolatey\\bin\\choco.exe)',
+    unless    => '$(Test-Path -Path C:\\ProgramData)',
     provider  => powershell,
     logoutput => true,
   }
