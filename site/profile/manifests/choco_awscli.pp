a1d7912ef7d78b5d9efb37a1d18d12f520232f9f
PowerSix <sinisam@gmail.com>
2018-03-29 14:35:03 +0100

    choco unless

--- site/profile/manifests/choco_awscli.pp
+++ site/profile/manifests/choco_awscli.pp
@@ -1,6 +1,7 @@
 class profile::choco_awscli {
   exec { 'choco_awscli':
     command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install awscli -y)',
+    unless    => '$(Test-Path -Path "C:\\Program Files\\Amazon\\AWSCLI\\aws.exe")',
     provider  => powershell,
     logoutput => true,
   }
--- site/profile/manifests/choco_firefox.pp
+++ site/profile/manifests/choco_firefox.pp
@@ -1,6 +1,7 @@
 class profile::choco_firefox {
   exec { 'choco_firefox':
     command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install firefox -y)',
+    unless    => '$(Test-Path -Path "C:\\Program Files\\Mozilla Firefox\\firefox.exe")',
     provider  => powershell,
     logoutput => true,
   }
