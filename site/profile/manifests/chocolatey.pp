2f4e8ebab0058cd6845c0c2766b017fff2913261
PowerSix <sinisam@gmail.com>
2018-03-29 14:38:29 +0100

    choco unless

--- site/profile/manifests/chocolatey.pp
+++ site/profile/manifests/chocolatey.pp
@@ -2,7 +2,7 @@ class profile::chocolatey {
   exec { 'chocolatey':
     # [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\\ProgramData\\chocolatey\\bin"
     command   => '$(Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); Exit 0)',
-    unless    => '$(Test-Path -Path C:\\ProgramData\\chocolatey\\bin\\choco.exe)',
+    # unless    => '$(Test-Path -Path C:\\ProgramData\\chocolatey\\bin\\choco.exe)',
     provider  => powershell,
     logoutput => true,
   }
