c614101ecef58ae2e12f0fea93b313e667ef6c25
PowerSix <sinisam@gmail.com>
2018-03-29 14:36:36 +0100

    choco unless

--- site/profile/manifests/choco_googlechrome.pp
+++ site/profile/manifests/choco_googlechrome.pp
@@ -1,7 +1,7 @@
 class profile::choco_googlechrome {
   exec { 'choco_googlechrome':
     command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install googlechrome -y)',
-    unless    => '$(Test-Path -Path "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"")',
+    unless    => '$(Test-Path -Path "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")',
     provider  => powershell,
     logoutput => true,
   }
