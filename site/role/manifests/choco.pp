c59de8128121ccce50541da6e482c9101686e961
PowerSix <sinisam@gmail.com>
2018-03-29 16:17:59 +0100

    commit

---
+++ site/role/manifests/choco.pp
@@ -0,0 +1,9 @@
+class role::choco {
+  exec { 'install_choco':
+    command   => '$(iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\')); Exit 0)',
+    provider  => powershell,
+    logoutput => true,
+	unless    => '$(Test-Path -Path C:\\ProgramData\\chocolatey\\bin\\choco.exe)',
+
+ }
+}
