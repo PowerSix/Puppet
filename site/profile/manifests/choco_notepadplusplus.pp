8cdd774ae7da9cabeaefe391c21c0740b9a373b5
PowerSix <sinisam@gmail.com>
2018-03-29 14:31:38 +0100

    choco

--- site/profile/manifests/choco_notepadplusplus.pp
+++ site/profile/manifests/choco_notepadplusplus.pp
@@ -1,6 +1,7 @@
 class profile::choco_notepadplusplus {
   exec { 'choco_notepadplusplus':
     command   => '$(C:\\ProgramData\\chocolatey\\bin\\choco install notepadplusplus -y)',
+    unless    => '$(Test-Path -Path "C:\\Program Files\\Notepad++\\notepad++.exe")',
     provider  => powershell,
     logoutput => true,
   }
