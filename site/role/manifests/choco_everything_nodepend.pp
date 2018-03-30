5f0d72159e6b56495b6f14ffa34e1f0d88db8c38
Mikasinovic <mikasino@amazon.com>
2018-03-30 13:50:25 +0100

    choco dependency test

---
+++ site/role/manifests/choco_everything_nodepend.pp
@@ -0,0 +1,9 @@
+class role::choco_everything_nodepend {
+  include profile::chocolatey
+  include profile::choco_awscli
+  include profile::choco_firefox
+  include profile::choco_googlechrome
+  include profile::choco_jdk8
+  include profile::choco_notepadplusplus
+  include profile::choco_tomcat
+}
