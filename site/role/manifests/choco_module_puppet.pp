c9aeb6941c89062a04266fe1f76b1d17e4614d70
Mikasinovic <mikasino@amazon.com>
2018-04-02 12:10:54 +0100

    choco

--- site/role/manifests/choco_module_puppet.pp
+++ site/role/manifests/choco_module_puppet.pp
@@ -1,6 +1,6 @@
 class role::choco_module_puppet {
   include chocolatey
-  $packages = ['notepadplusplus', '7zip', 'awscli', 'pandoc']
+  $packages = ['notepadplusplus', '7zip', 'awscli', 'pandoc', 'firefox', 'googlechrome']
   package { $packages:
     ensure   => installed,
     provider => 'chocolatey',
