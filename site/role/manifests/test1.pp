793d33fd40338cf74d829ea221bf3905d8824b08
PowerSix <sinisam@gmail.com>
2018-04-10 17:41:49 +0100

    test 2

--- site/role/manifests/test1.pp
+++ site/role/manifests/test1.pp
@@ -1,11 +1,11 @@
-$data = @(DATA)/
+$data = @(EOT)/
   -Dcatalina.home=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\r\n
   -Dcatalina.base=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\r\n
   -Djava.io.tmpdir=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\temp\r\n
   -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager\r\n
   -Djava.util.logging.config.file=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\conf\\logging.properties\r\n
   -XX:MaxPermSize=256m
-  | DATA
+  | EOT
 
 class role::test1 {
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Apache Software Foundation\Procrun 2.0\Tomcat8\Parameters\Java\Options':
