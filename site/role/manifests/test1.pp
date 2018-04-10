34b12469600558f34525a084a3916718f4ed27a9
PowerSix <sinisam@gmail.com>
2018-04-10 17:08:58 +0100

    test 2

--- site/role/manifests/test1.pp
+++ site/role/manifests/test1.pp
@@ -1,12 +1,14 @@
+$data = "-Dcatalina.home=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12",
+        "-Dcatalina.base=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12",
+        "-Djava.io.tmpdir=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\temp",
+        "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager",
+        "-Djava.util.logging.config.file=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\conf\\logging.properties",
+        "-XX:MaxPermSize=256m"
+
 class role::test1 {
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Apache Software Foundation\Procrun 2.0\Tomcat8\Parameters\Java\Options':
   ensure => present,
   type   => array,
-  data   => "-Dcatalina.home=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12",
-            "-Dcatalina.base=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12",
-            "-Djava.io.tmpdir=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\temp",
-            "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager",
-            "-Djava.util.logging.config.file=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\\conf\\logging.properties",
-            "-XX:MaxPermSize=256m",
+  data   => $data,
   }
 }
