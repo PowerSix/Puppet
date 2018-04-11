e798875bf9b6b65b645ed3cc35bb8580032cc3cf
PowerSix <sinisam@gmail.com>
2018-04-11 13:21:16 +0100

    change

--- site/role/manifests/registry_value.pp
+++ site/role/manifests/registry_value.pp
@@ -1,5 +1,18 @@
-class role::test1 {
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Apache Software Foundation\Procrun 2.0\Tomcat8\Parameters\Java\Options':
+class role::registry_value {
+
+  service { 'tomcat8':
+    ensure => running,
+    enable => true,
+  } ->
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Test\JvmMs':
+    ensure => present,
+    type   => dword,
+    data   => "256",
+    notify => Service['tomcat8'],
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Test\Options':
   ensure => present,
   type   => array,
   data   => "-Dcatalina.home=C:\\Program Files\\Apache Software Foundation\\tomcat\\apache-tomcat-8.5.12\r\n
