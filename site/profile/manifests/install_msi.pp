5a828017eed1a48c015190678389df0504e50a38
PowerSix <sinisam@gmail.com>
2018-05-11 11:36:02 +0100

    2018-05-11 11:36:02

--- site/profile/manifests/install_msi.pp
+++ site/profile/manifests/install_msi.pp
@@ -2,7 +2,7 @@ class profile::install_msi {
   package { 'newrelic-infra':
     ensure          => 'installed',
     provider        => 'windows',
-    source          => 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.msi',
+    source          => 'http://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.0.703.msi',
     install_options => ['/quiet'],
   }
 }
