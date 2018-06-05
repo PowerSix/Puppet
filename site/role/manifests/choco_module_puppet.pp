89eeb574b9c56e3066843218cd93f8e594d577e7
PowerSix <sinisam@gmail.com>
2018-06-05 11:13:32 +0100

    2018-06-05 11:13:32

--- site/role/manifests/choco_module_puppet.pp
+++ site/role/manifests/choco_module_puppet.pp
@@ -1,6 +1,6 @@
 class role::choco_module_puppet {
   include chocolatey
-  $packages = ['notepadplusplus', '7zip', 'awscli', 'pandoc', 'firefox', 'googlechrome']
+  $packages = ['7zip', 'awscli', 'firefox', 'googlechrome', 'notepadplusplus', 'sysinternals', 'tightvnc', 'treesizefree', 'visualstudiocode']
   package { $packages:
     ensure   => installed,
     provider => 'chocolatey',
