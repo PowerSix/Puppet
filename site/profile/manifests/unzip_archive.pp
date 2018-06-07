9f2e9bb03649806d95c6eafd1c31b822740331cc
PowerSix <sinisam@gmail.com>
2018-06-08 00:26:15 +0100

    2018-06-08 00:26:15

--- site/profile/manifests/unzip_archive.pp
+++ site/profile/manifests/unzip_archive.pp
@@ -1,8 +1,8 @@
 class profile::unzip_archive {
-  archive { 'C:\\Program\ Files\\QAS.zip':
+  archive { 'C:\Program Files\QAS.zip':
     source       => 'https://s3-us-west-2.amazonaws.com/opsworks-installs/QAS.zip',
     extract      => true,
-    extract_path => "C:\\Program Files\\",
+    extract_path => "C:\\Program Files",
     creates      => "C:\\Program Files\\QAS",
   }
 }
